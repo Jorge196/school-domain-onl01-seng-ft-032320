48f9c1fe8ec48de926df803ca2a8bfe6a76886ea
Jorge Ramirez <joramirez96@gmail.com>
2020-04-02 21:28:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,25 +1,10 @@
 # code here!
 class School
-  attr_accessor :roster
+  attr_accessor :roster, :student
 
   def initialize(student)
     @student = student
     @roster = []
   end
 
-  def student=(student)
-    @student = student
-  end
-
-  # def roster=(roster)
-  #   @roster = roster
-  # end
-  #
-  # def roster
-  #   @roster
-  # end
-
-  def student
-    @student
-  end
-end
+end 
