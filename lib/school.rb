5e803247148c8c2a9a114c123f50486fda406bd6
Jorge Ramirez <joramirez96@gmail.com>
2020-04-02 21:18:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+class School
+  def initialize(student)
+    @student = student
+  end
+
+  def student=(student)
+    @student = student
+  end
+
+  def student
+    @student
+  end
+end
